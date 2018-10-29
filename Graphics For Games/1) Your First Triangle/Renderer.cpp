f2327918a0e3b41a229f98d2f2597c9f11109582
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-29 15:06:41 +0000

    Implemented renderer constructor and destructor

--- Graphics For Games/1) Your First Triangle/Renderer.cpp
+++ Graphics For Games/1) Your First Triangle/Renderer.cpp
@@ -2,11 +2,23 @@
 
 
 
-Renderer::Renderer()
+Renderer::Renderer(Window& parent):
+	OGLRenderer(parent)
 {
+	triangle = Mesh::GenerateTriangle();
+
+	currentShader = new Shader("../../Shaders/basicVertex.glsl",
+		"../../Shader/colourFragment.glsl");
+
+	if (!currentShader->LinkProgram) {
+		return;
+	}
+
+	init = true;
 }
 
 
 Renderer::~Renderer()
 {
+	delete triangle;
 }
--- Graphics For Games/1) Your First Triangle/Renderer.h
+++ Graphics For Games/1) Your First Triangle/Renderer.h
@@ -4,7 +4,7 @@
 class Renderer : public OGLRenderer
 {
 public:
-	Renderer(Window &parent);
+	Renderer(Window& parent);
 	virtual ~Renderer(void);
 	virtual void RenderScene();
 
