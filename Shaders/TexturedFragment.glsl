5ba7d510990a8d415e5d3f7bc96f041b90b9347b
sbendell <43750640+sbendell@users.noreply.github.com>
2018-11-01 16:52:28 +0000

    Revert "Revert "Revert "Shader Changes"""
    This reverts commit 3bb86dafc4bcd36000c88b6d39d38b1188ae6e1a.

--- Shaders/TexturedFragment.glsl
+++ Shaders/TexturedFragment.glsl
@@ -1,15 +1,13 @@
 #version 150
 
 uniform sampler2D diffuseTex;
-uniform float 
 
 in Vertex {
 	vec2 texCoord;
-	vec4 colour;
 } IN;
 
 out vec4 fragColor;
 
 void main(void){
-	fragColor = texture(diffuseTex, IN.texCoord) * IN.colour;
+	fragColor = texture(diffuseTex, IN.texCoord);
 }
