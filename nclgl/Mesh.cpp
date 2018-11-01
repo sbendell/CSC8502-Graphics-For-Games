d91d94687f489401599c0958b986feec1e608140
sbendell <43750640+sbendell@users.noreply.github.com>
2018-11-01 17:12:50 +0000

    Created generateQuad function

--- nclgl/Mesh.cpp
+++ nclgl/Mesh.cpp
@@ -49,24 +49,28 @@ Mesh* Mesh::GenerateTriangle() {
 	return m;
 }
 
-Mesh* Mesh::GenerateSquare() {
+Mesh* Mesh::GenerateQuad() {
 	Mesh* m = new Mesh();
-	m->numVertices = 6;
+	m->numVertices = 4;
+	m->type = GL_TRIANGLE_STRIP;
+
 	m->vertices = new Vector3[m->numVertices];
-	m->vertices[0] = Vector3(-0.5f, 0.5f, 0.0f);
-	m->vertices[1] = Vector3(0.5f, 0.5f, 0.0f);
-	m->vertices[2] = Vector3(-0.5f, -0.5f, 0.0f);
-	m->vertices[3] = Vector3(0.5f, 0.5f, 0.0f);
-	m->vertices[4] = Vector3(-0.5f, -0.5f, 0.0f);
-	m->vertices[5] = Vector3(0.5f, -0.5f, 0.0f);
+	m->vertices[0] = Vector3(-1.0f, -1.0f, 0.0f);
+	m->vertices[1] = Vector3(-1.0f, 1.0f, 0.0f);
+	m->vertices[2] = Vector3(1.0f, -1.0f, 0.0f);
+	m->vertices[3] = Vector3(1.0f, 1.0f, 0.0f);
+
+	m->textureCoords = new Vector2[m->numVertices];
+	m->textureCoords[0] = Vector2(0.0f, 1.0f);
+	m->textureCoords[1] = Vector2(0.0f, 0.0f);
+	m->textureCoords[2] = Vector2(1.0f, 1.0f);
+	m->textureCoords[3] = Vector2(1.0f, 0.0f);
 
 	m->colours = new Vector4[m->numVertices];
-	m->colours[0] = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
-	m->colours[1] = Vector4(0.0f, 1.0f, 0.0f, 1.0f);
-	m->colours[2] = Vector4(0.0f, 0.0f, 1.0f, 1.0f);
-	m->colours[3] = Vector4(0.0f, 1.0f, 0.0f, 1.0f);
-	m->colours[4] = Vector4(0.0f, 0.0f, 1.0f, 1.0f);
-	m->colours[5] = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+	for (int i = 0; i < m->numVertices; i++)
+	{
+		m->colours[i] = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+	}
 
 	m->BufferData();
 	return m;
