341ab5d548947cef26cb859dfd20420bc56e5b8b
sbendell <43750640+sbendell@users.noreply.github.com>
2018-11-06 14:24:23 +0000

    Implemented Plane constructor

--- nclgl/Plane.cpp
+++ nclgl/Plane.cpp
@@ -1,12 +1,13 @@
 #include "Plane.h"
 
+Plane::Plane(const Vector3& normal, float distance, bool normalise) {
+	if (normalise) {
+		float length = sqrt(Vector3::Dot(normal, normal));
 
-
-Plane::Plane()
-{
-}
-
-
-Plane::~Plane()
-{
-}
+		this->normal = normal / length;
+		this->distance = distance / length;
+	}
+	else {
+		this->normal = normal;
+		this->distance = distance;
+	}}
