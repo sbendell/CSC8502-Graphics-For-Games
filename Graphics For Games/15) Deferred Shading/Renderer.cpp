55c3e72e5527d5cb70eade24a54cfac87e257805
sbendell <43750640+sbendell@users.noreply.github.com>
2018-11-15 15:32:25 +0000

    Implemented DrawLights function

--- Graphics For Games/15) Deferred Shading/Renderer.cpp
+++ Graphics For Games/15) Deferred Shading/Renderer.cpp
@@ -125,3 +125,100 @@ void Renderer::GenerateScreenTexture(GLuint & into, bool depth) {
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
+
+void Renderer::UpdateScene(float msec) {
+	camera->UpdateCamera(msec);
+	viewMatrix = camera->BuildViewMatrix();
+	rotation = msec * 0.01f;
+}
+
+void Renderer::RenderScene() {
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	FillBuffers();
+	DrawPointLights();
+	CombineBuffers();
+	SwapBuffers();}void Renderer::FillBuffers() {
+	glBindFramebuffer(GL_FRAMEBUFFER, bufferFBO);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	SetCurrentShader(sceneShader);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"diffuseTex"), 0);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"bumpTex"), 1);
+
+	projMatrix = Matrix4::Perspective(1.0f, 10000.0f,
+		(float)width / (float)height, 45.0f);
+	modelMatrix.ToIdentity();
+	UpdateShaderMatrices();
+
+	heightMap->Draw();
+
+	glUseProgram(0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);}void Renderer::DrawPointLights() {
+	SetCurrentShader(pointlightShader);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, pointLightFBO);
+
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBlendFunc(GL_ONE, GL_ONE);
+
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"depthTex"), 3);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(),
+		"normTex"), 4);
+
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, bufferDepthTex);
+
+	glActiveTexture(GL_TEXTURE4);
+	glBindTexture(GL_TEXTURE_2D, bufferNormalTex);
+
+	glUniform3fv(glGetUniformLocation(currentShader->GetProgram(),
+		"cameraPos"), 1, (float *)& camera->GetPosition());
+
+	glUniform2f(glGetUniformLocation(currentShader->GetProgram(),
+		"pixelSize"), 1.0f / width, 1.0f / height);	Vector3 translate = Vector3((RAW_HEIGHT * HEIGHTMAP_X / 2.0f), 500,
+		(RAW_HEIGHT * HEIGHTMAP_Z / 2.0f));
+
+	Matrix4 pushMatrix = Matrix4::Translation(translate);
+	Matrix4 popMatrix = Matrix4::Translation(-translate);
+
+	for (int x = 0; x < LIGHTNUM; ++x) {
+		for (int z = 0; z < LIGHTNUM; ++z) {
+			Light & l = pointLights[(x * LIGHTNUM) + z];
+			float radius = l.GetRadius();
+
+			modelMatrix =
+				pushMatrix *
+				Matrix4::Rotation(rotation, Vector3(0, 1, 0)) *
+				popMatrix *
+				Matrix4::Translation(l.GetPosition()) *
+				Matrix4::Scale(Vector3(radius, radius, radius));
+
+			l.SetPosition(modelMatrix.GetPositionVector());
+
+			SetShaderLight(l);
+
+			UpdateShaderMatrices();
+
+			float dist = (l.GetPosition() - camera->GetPosition()).Length();
+			if (dist < radius) {// camera is inside the light volume !
+				glCullFace(GL_FRONT);
+			}
+			else {
+				glCullFace(GL_BACK);
+			}
+
+			sphere->Draw();
+		}	}	glCullFace(GL_BACK);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glClearColor(0.2f, 0.2f, 0.2f, 1);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glUseProgram(0);}
